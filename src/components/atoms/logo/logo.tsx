a0edee55dff84782d04999ada3e9b86148ca6a33
Nadya Karandasheva <55376506+Nadya-Karandasheva@users.noreply.github.com>
2025-01-03 01:22:52 +0300

    Update and rename Logo.tsx to logo.tsx

--- src/components/atoms/logo/logo.tsx
+++ src/components/atoms/logo/logo.tsx
@@ -6,7 +6,7 @@ import { AppRoute } from '../../../emuns/app-route.emun';
  * Компонент логотипа.
  * @returns {JSX.Element}
  */
-export const Logo = () => (
+export const logo = () => (
   <Link className="header__logo-link header__logo-link--active" to={AppRoute.Main}>
     <img className="header__logo" src="/img/logo.svg" alt="6 cities logo" width="81" height="41" />
   </Link>
